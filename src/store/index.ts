bcf89e043323a9c2ad981667388fa98435900a10
Oleksandra Nedashkivska <alexandranedashkovskaya10@gmail.com>
2024-04-24 22:59:06 +0300

    add middleware to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import currentRepoSlice from "./currentRepoSlice";
 import issuesSlice, { changeLimit, changeIssueStatus } from "./issuesSlice";
+import listenerMiddleware from "./middlewares";
 import { fetchRepoData } from "./thunks";
 
+export const reducer = {
+  currentRepo: currentRepoSlice.reducer,
+  issues: issuesSlice.reducer,
+};
+
+const middleware = (getDefaultMiddleware) =>
+  getDefaultMiddleware().prepend(listenerMiddleware.middleware);
+
 const store = configureStore({
-  reducer: {
-    currentRepo: currentRepoSlice.reducer,
-    issues: issuesSlice.reducer,
-  },
+  reducer,
+  middleware,
 });
 
 export { fetchRepoData, changeLimit, changeIssueStatus };
